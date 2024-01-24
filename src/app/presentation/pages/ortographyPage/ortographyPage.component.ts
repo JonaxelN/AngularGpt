857c48cab05576a18ecd503ef482e546c1bb6a41
JonaxelN <jonathanaxelnunez@gmail.com>
2024-01-23 18:54:26 -0600

    ChatBubbles

--- src/app/presentation/pages/ortographyPage/ortographyPage.component.ts
+++ src/app/presentation/pages/ortographyPage/ortographyPage.component.ts
@@ -1,11 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChatMessageComponent } from '../../components/chat-bubbles/chatMessage/chatMessage.component';
+import { MyMessageComponent } from '../../components/chat-bubbles/myMessage/myMessage.component';
 
 @Component({
   selector: 'app-ortography-page',
   standalone: true,
   imports: [
     CommonModule,
+    ChatMessageComponent,
+    MyMessageComponent
   ],
   templateUrl: './ortographyPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
