f07c8fe8760f3d44fe6e31284aa74cedae8aafeb
JonaxelN <jonathanaxelnunez@gmail.com>
2024-01-23 19:01:55 -0600

    alias components

---
+++ src/app/presentation/components/index.ts
@@ -0,0 +1,3 @@
+export * from '@components/chat-bubbles/chatMessage/chatMessage.component';
+export * from '@components/chat-bubbles/myMessage/myMessage.component';
+export * from '@components/sidebarMenuItem/sidebarMenuItem.component';
